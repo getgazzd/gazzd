3ac46ed3e2e40e2fa661c7c80036b706f3574960
getgazzd <getgazzd@gmail.com>
2024-10-03 20:15:20 +0500

    fix: remove anonymous fun

--- components/Layouts/PageLayout.tsx
+++ components/Layouts/PageLayout.tsx
@@ -1,7 +1,5 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { NextSeo } from "next-seo";
-import Script from 'next/script';
-import Head from 'next/head'
 import { OpenGraph, OpenGraphMedia } from "next-seo/lib/types";
 import React, { ReactNode } from "react";
 
@@ -59,35 +57,6 @@ function Layout({
         description={description}
         openGraph={openGraph}
       />
-
-      <Script>
-      !function(f, b, e, v, n, t, s)
-      {
-      	if (f.fbq) return;
-      	n = f.fbq = function ()
-      	{
-      		n.callMethod ?
-      			n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-      	};
-      	if (!f._fbq) f._fbq = n;
-      	n.push = n;
-      	n.loaded = !0;
-      	n.version = '2.0';
-      	n.queue = [];
-      	t = b.createElement(e);
-      	t.async = !0;
-      	t.src = v;
-      	s = b.getElementsByTagName(e)[0];
-      	s.parentNode.insertBefore(t, s)
-      }(window, document, 'script',
-      	'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '891494069704943');
-      fbq('track', 'PageView');
-      </Script>
-      <Head>
-        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=891494069704943&ev=PageView&noscript=1" /></noscript>
-      </Head>
-      
 
       <AnimatePresence exitBeforeEnter>
         <motion.main {...animation} key={title + "GAZZD"}>
